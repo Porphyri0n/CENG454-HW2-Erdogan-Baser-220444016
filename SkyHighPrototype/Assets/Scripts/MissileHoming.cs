59e858b9342b2cb1e58b495ff2728023348ed4e1
Erdoğan Başer <eb.baser@gmail.com>
2026-04-07 10:11:47 +0300

    feat: setup missile launcher base and homing placeholder

---
+++ SkyHighPrototype/Assets/Scripts/MissileHoming.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MissileHoming : MonoBehaviour
+{
+    // Aþama 2'de içini dolduracaðýz. Þimdilik hata vermemesi için boþ býrakýyoruz.
+    public void SetTarget(Transform newTarget)
+    {
+
+    }
+}
